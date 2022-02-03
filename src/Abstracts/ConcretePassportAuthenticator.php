f76c3f359a682c14b644eef69f14c3a0c5d060ca
Remy Kooistra <7780504+BarryPooter@users.noreply.github.com>
2022-02-03 15:22:52 +0100

    Update according to inbouw fixes

--- src/Abstracts/ConcretePassportAuthenticator.php
+++ src/Abstracts/ConcretePassportAuthenticator.php
@@ -2,7 +2,6 @@
 
 use Keukenmagazijn\PassportAuthenticator\Contracts\SendsRequests;
 use Keukenmagazijn\PassportAuthenticator\Instances\Authenticator;
-use Keukenmagazijn\PassportAuthenticator\Entities\ExternalOauth2Credential;
 use GuzzleHttp\Client;
 
 abstract class ConcretePassportAuthenticator implements SendsRequests
@@ -22,7 +21,8 @@ abstract class ConcretePassportAuthenticator implements SendsRequests
      * @return array
      * @throws \Exception
      */
-    final public function get(string $endpoint, array $dataToSend = [], array $extraHeaders = [], bool $debugOutput = false, bool $generateJsonContentType = true): array {
+    final public function get(string $endpoint, array $dataToSend = [], array $extraHeaders = [], bool $debugOutput = false, bool $generateJsonContentType = true): array
+    {
         if (!$debugOutput) {
             if (!$this->isAuthenticated()) $this->_authenticate();
         }
@@ -37,7 +37,8 @@ abstract class ConcretePassportAuthenticator implements SendsRequests
      * @return array
      * @throws \Exception
      */
-    final public function post(string $endpoint, array $dataToSend = [], array $extraHeaders = [], bool $debugOutput = false, bool $generateJsonContentType = true): array {
+    final public function post(string $endpoint, array $dataToSend = [], array $extraHeaders = [], bool $generateJsonContentType = true, bool $debugOutput = false): array
+    {
         if (!$debugOutput) {
             if (!$this->isAuthenticated()) $this->_authenticate();
         }
@@ -48,22 +49,25 @@ abstract class ConcretePassportAuthenticator implements SendsRequests
      * @return void
      * @throws \Exception
      */
-    private function _authenticate(): void {
+    private function _authenticate(): void
+    {
         $this->_getAuthenticator()->authorize();
     }
 
     /**
      * @return bool
      */
-    public function isAuthenticated (): bool {
+    public function isAuthenticated(): bool
+    {
         return $this->getAuthenticator()
-            ->currentlyHasAToken() && !$this->getAuthenticator()->isTokenExpired();
+                ->currentlyHasAToken() && !$this->getAuthenticator()->isTokenExpired();
     }
 
     /**
      * @return Authenticator
      */
-    protected function getAuthenticator(): Authenticator {
+    protected function getAuthenticator(): Authenticator
+    {
         if (empty($this->_oauth2Authenticator)) {
             $this->_oauth2Authenticator = $this->getAuthenticatorInstance();
         }
@@ -90,12 +94,13 @@ abstract class ConcretePassportAuthenticator implements SendsRequests
      * @param array $extraHeaders
      * @return array
      */
-    protected function sendGetRequest(string $endpoint, array $dataToSend = [], array $extraHeaders = [], bool $generateJsonContentType = true): array {
+    protected function sendGetRequest(string $endpoint, array $dataToSend = [], array $extraHeaders = [], bool $generateJsonContentType = true): array
+    {
         $_sendableData = array_merge($this->getHeaders($extraHeaders, $generateJsonContentType), $dataToSend);
         $_clientCall = $this->_getGuzzleClient()
             ->get($endpoint, array_merge($_sendableData, $extraHeaders));
-        $_returnData = json_decode((string) $_clientCall->getBody());
-        if (is_object($_returnData)) $_returnData = (array) $_returnData;
+        $_returnData = json_decode((string)$_clientCall->getBody());
+        if (is_object($_returnData)) $_returnData = (array)$_returnData;
         return $_returnData;
     }
 
@@ -105,12 +110,13 @@ abstract class ConcretePassportAuthenticator implements SendsRequests
      * @param array $extraHeaders
      * @return array
      */
-    protected function sendPostRequest(string $endpoint, array $dataToSend = [], array $extraHeaders = [], bool $generateJsonContentType = true): array {
+    protected function sendPostRequest(string $endpoint, array $dataToSend = [], array $extraHeaders = [], bool $generateJsonContentType = true): array
+    {
         $_sendableData = array_merge($this->getHeaders($extraHeaders, $generateJsonContentType), $dataToSend);
         $_clientCall = $this->_getGuzzleClient()
             ->post($endpoint, array_merge($_sendableData, $extraHeaders));
-        $_returnData = json_decode((string) $_clientCall->getBody());
-        if (is_object($_returnData)) $_returnData = (array) $_returnData;
+        $_returnData = json_decode((string)$_clientCall->getBody());
+        if (is_object($_returnData)) $_returnData = (array)$_returnData;
         return $_returnData;
     }
 
@@ -121,7 +127,8 @@ abstract class ConcretePassportAuthenticator implements SendsRequests
      * @param bool $generateJsonContentType
      * @return array
      */
-    public function getHeaders(?array $extraHeaders, bool $skipAccessToken = false, bool $generateJsonContentType = true): array {
+    public function getHeaders(?array $extraHeaders,  bool $generateJsonContentType = true, bool $skipAccessToken = false): array
+    {
         $_accessToken = $skipAccessToken ? null : $this->_getAuthenticator()->getCredentials()->access_token;
         $headers = [
             'Authorization' => sprintf("Bearer %s", $_accessToken),
@@ -140,19 +147,21 @@ abstract class ConcretePassportAuthenticator implements SendsRequests
     /**
      * @return Client
      */
-    protected final function _getGuzzleClient() : Client {
+    protected final function _getGuzzleClient(): Client
+    {
         if (empty($this->_guzzleClient)) {
             $this->_guzzleClient = new \GuzzleHttp\Client([
                 'base_uri' => $this->getBaseUri()
             ]);
         }
-	return $this->_guzzleClient;
+        return $this->_guzzleClient;
     }
 
     /**
      * @return Authenticator
      */
-    private function _getAuthenticator(): Authenticator {
+    private function _getAuthenticator(): Authenticator
+    {
         if (empty($this->_oauth2Authenticator)) {
             $this->_oauth2Authenticator = $this->getAuthenticator();
         }
